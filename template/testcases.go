3aefa356586fbe1fc285c6ac11ea5f9cf3b221d7
Oskar Jansson <bullsaw1@gmail.com>
2020-11-30 15:00:07 +0100

    add: solution template

---
+++ template/testcases.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	tc "github.com/IAmBullsaw/AOC-2020/pkg/testcase"
+)
+
+var testcases = tc.TestCases {
+	{
+		`1`, // input
+		`1`, // expected lvl 1
+		`2`, // expected lvl 2
+	},
+}
