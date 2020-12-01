a0a7406be1e782798492edd5f2fd6dea4fe878ec
ezjanos <oskar.jansson@ericsson.com>
2020-12-01 10:06:16 +0100

    adds: package inp
    something to use to ease the handling of input text

---
+++ pkg/inp/inp.go
@@ -0,0 +1,24 @@
+package inp
+
+import (
+	"strings"
+	"strconv"
+)
+
+// ToInts return a slice of ints from passed in text
+func ToInts(text string) (result []int) {
+	for _, a := range strings.Fields(text) {
+		ai, _ := strconv.Atoi(a)
+		result = append(result, ai)
+	}
+	return
+}
+
+// ToIntsFunc return a slice of ints from passed in text, split on f
+func ToIntsFunc(text string, f func(rune) bool) (result []int) {
+	for _, a := range strings.FieldsFunc(text, f) {
+		ai, _ := strconv.Atoi(a)
+		result = append(result, ai)
+	}
+	return
+}
