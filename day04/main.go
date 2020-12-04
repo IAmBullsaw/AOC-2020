52a93ae71e363b4d1d3b3106080d16617f069078
ezjanos <oskar.jansson@ericsson.com>
2020-12-04 11:15:29 +0100

    updates: day04 is cleaner

--- day04/main.go
+++ day04/main.go
@@ -10,22 +10,24 @@ import (
 	"github.com/IAmBullsaw/AOC-2020/pkg/execution"
 )
 
-// solutionLvl1 return answers for level 1
-func solutionLvl1(puzzle string) (answer int) {
-	pps := []map[string]string{}
+// getInput return a list of maps corresponding to one passport
+func getInput(puzzle string) (pps []map[string]string) {
 	pp := map[string]string{}
-	for _, data := range strings.Split(puzzle, "\n") {
+	for _, data := range strings.Split(puzzle, "\n\n") {
 
 		for _, values := range strings.Fields(data) {
 			kv := strings.Split(values, ":")
 			pp[kv[0]] = kv[1]
 		}
-
-		if len(data) == 0 {
-			pps = append(pps, pp)
-			pp = map[string]string{}
-		}
+		pps = append(pps, pp)
+		pp = map[string]string{}
 	}
+	return
+}
+
+// solutionLvl1 return answers for level 1
+func solutionLvl1(puzzle string) (answer int) {
+	pps := getInput(puzzle)
 
 	fields := []string{
 		"byr",
@@ -44,6 +46,7 @@ func solutionLvl1(puzzle string) (answer int) {
 			if passport[f] == "" {
 				if f != "cid" {
 					valid = false
+					break
 				}
 
 			}
@@ -57,81 +60,65 @@ func solutionLvl1(puzzle string) (answer int) {
 	return
 }
 
+// Policy has a Rule pattern which match should adhere to the Validate function
 type Policy struct {
 	Rule     *regexp.Regexp
 	Validate func(string) bool
 }
 
+// Predetermined Policies which each Passport field must follow (cid excluded)
+var Policies = map[string]Policy{
+	"byr": Policy{Rule: regexp.MustCompile(`^\d{4}$`), Validate: func(data string) bool {
+		val, _ := strconv.Atoi(data)
+		return cmp.InBounds(1920, val, 2002, true) == cmp.Within
+	}},
+	"iyr": Policy{Rule: regexp.MustCompile(`^\d{4}$`), Validate: func(data string) bool {
+		val, _ := strconv.Atoi(data)
+		return cmp.InBounds(2010, val, 2020, true) == cmp.Within
+	}},
+	"eyr": Policy{Rule: regexp.MustCompile(`^\d{4}$`), Validate: func(data string) bool {
+		val, _ := strconv.Atoi(data)
+		return cmp.InBounds(2020, val, 2030, true) == cmp.Within
+	}},
+	"hgt": Policy{Rule: regexp.MustCompile(`^\d+cm$|^\d+in$`), Validate: func(data string) bool {
+		val, _ := strconv.Atoi(data[:len(data)-2])
+		if data[len(data)-2:] == "cm" {
+			return cmp.InBounds(150, val, 193, true) == cmp.Within
+		} else {
+			return cmp.InBounds(59, val, 76, true) == cmp.Within
+		}
+	}},
+	"hcl": Policy{Rule: regexp.MustCompile(`^#\w{6}$`), Validate: func(data string) bool { return true }},
+	"ecl": Policy{Rule: regexp.MustCompile(`^\w{3}$`), Validate: func(data string) bool {
+		colors := []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+		for _, color := range colors {
+			if data == color {
+				return true
+			}
+		}
+		return false
+	}},
+	"pid": Policy{Rule: regexp.MustCompile(`^\d{9}$`), Validate: func(data string) bool { return true }},
+}
+
 // solutionLvl2 return answers for level 2
 func solutionLvl2(puzzle string) (answer int) {
-	pps := []map[string]string{}
-	pp := map[string]string{}
-	for _, data := range strings.Split(puzzle, "\n") {
-
-		for _, values := range strings.Fields(data) {
-			kv := strings.Split(values, ":")
-			pp[kv[0]] = kv[1]
-		}
-
-		if len(data) == 0 {
-			pps = append(pps, pp)
-			pp = map[string]string{}
-		}
-	}
-
-	fields := map[string]Policy{
-		"byr": Policy{Rule: regexp.MustCompile(`^\d{4}$`), Validate: func(data string) bool {
-			val, _ := strconv.Atoi(data)
-			return cmp.InBounds(1920, val, 2002, true) == cmp.Within
-		}},
-		"iyr": Policy{Rule: regexp.MustCompile(`^\d{4}$`), Validate: func(data string) bool {
-			val, _ := strconv.Atoi(data)
-			return cmp.InBounds(2010, val, 2020, true) == cmp.Within
-		}},
-		"eyr": Policy{Rule: regexp.MustCompile(`^\d{4}$`), Validate: func(data string) bool {
-			val, _ := strconv.Atoi(data)
-			return cmp.InBounds(2020, val, 2030, true) == cmp.Within
-		}},
-		"hgt": Policy{Rule: regexp.MustCompile(`^\d+cm$|^\d+in$`), Validate: func(data string) bool {
-			val, _ := strconv.Atoi(data[:len(data)-2])
-			if data[len(data)-2:] == "cm" {
-				return cmp.InBounds(150, val, 193, true) == cmp.Within
-			} else {
-				return cmp.InBounds(59, val, 76, true) == cmp.Within
-			}
-		}},
-		"hcl": Policy{Rule: regexp.MustCompile(`^#\w{6}$`), Validate: func(data string) bool { return true }},
-		"ecl": Policy{Rule: regexp.MustCompile(`^\w{3}$`), Validate: func(data string) bool {
-			colors := []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
-			for _, color := range colors {
-				if data == color {
-					return true
-				}
-			}
-			return false
-		}},
-		"pid": Policy{Rule: regexp.MustCompile(`^\d{9}$`), Validate: func(data string) bool { return true }},
-		"cid": Policy{Rule: regexp.MustCompile(`.`), Validate: func(data string) bool { return true }},
-	}
+	pps := getInput(puzzle)
 
 	for _, passport := range pps {
 		valid := true
-		for key, policy := range fields {
-			if passport[key] == "" {
-				if key != "cid" {
-					valid = false
-				}
-			} else {
-				if !(policy.Rule.MatchString(passport[key]) && policy.Validate(passport[key])) {
-					valid = false
-				}
+		for key, policy := range Policies {
+			if len(passport) < 7 ||
+				(len(passport) == 7 && passport["cid"] != "") ||
+				passport[key] == "" ||
+				!(policy.Rule.MatchString(passport[key]) && policy.Validate(passport[key])) {
+				valid = false
+				break
 			}
-
 		}
 		if valid {
 			answer++
 		}
-
 	}
 
 	return
