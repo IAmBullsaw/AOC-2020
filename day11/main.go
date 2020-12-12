d8326322a9b6fc3920e3e87d905b74b85dc495d1
ezjanos <oskar.jansson@ericsson.com>
2020-12-13 00:51:41 +0100

    solves: day11

--- day11/main.go
+++ day11/main.go
@@ -123,8 +123,90 @@ func solutionLvl1(puzzle string, parameters map[string]int) (answer int) {
 
 }
 
+func adjacentSearch(px, py int, grid [][]int) (adj int) {
+	for dy := -1; dy <= 1; dy++ {
+		for dx := -1; dx <= 1; dx++ {
+			if dx == 0 && dy == 0 {
+				continue
+			}
+			x := px + dx
+			y := py + dy
+			for cmp.InBounds(0, x, len(grid[0])-1, true) == cmp.Within && cmp.InBounds(0, y, len(grid)-1, true) == cmp.Within && grid[y][x] == Floor {
+				x += dx
+				y += dy
+			}
+			if cmp.InBounds(0, x, len(grid[0])-1, true) == cmp.Within && cmp.InBounds(0, y, len(grid)-1, true) == cmp.Within {
+				// fmt.Println(grid[y][x])
+				if grid[y][x] == Occupied {
+					adj++
+				}
+			}
+
+		}
+	}
+	return
+}
+
+func step2(grid [][]int) ([][]int, bool) {
+	next := [][]int{}
+	changed := false
+	for y, row := range grid {
+		r := []int{}
+		for x, c := range row {
+			if c == Floor {
+				r = append(r, Floor)
+				continue
+			}
+			adj := adjacentSearch(x, y, grid)
+			// fmt.Println(x, y, "adj", adj)
+			if c == Free && adj == 0 {
+				r = append(r, Occupied)
+				changed = true
+			} else if c == Occupied && 5 <= adj {
+				r = append(r, Free)
+				changed = true
+			} else {
+				r = append(r, c)
+			}
+		}
+		next = append(next, r)
+	}
+	return next, changed
+}
+
 // solutionLvl2 return answers for level 2
 func solutionLvl2(puzzle string, parameters map[string]int) (answer int) {
+	grid := [][]int{}
+	for _, row := range strings.Fields(puzzle) {
+		r := []int{}
+		for _, c := range row {
+			if c == '.' {
+				r = append(r, Floor)
+			} else if c == 'L' {
+				r = append(r, Free)
+			} else if c == '#' {
+				r = append(r, Occupied)
+			}
+		}
+		grid = append(grid, r)
+	}
+
+	counter := 1
+	for {
+		next, changed := step2(grid)
+		if !changed {
+			for _, r := range next {
+				for _, s := range r {
+					if s == Occupied {
+						answer++
+					}
+				}
+			}
+			break
+		}
+		grid = next
+		counter++
+	}
 	return
 }
 
