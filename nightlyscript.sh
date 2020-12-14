3d93b933c54558707e2bb2060963c7174f46af52
ezjanos <oskar.jansson@ericsson.com>
2020-12-14 16:45:06 +0100

    adds: nightlyscript

---
+++ nightlyscript.sh
@@ -0,0 +1,11 @@
+YEARVAR=$(date "+%Y")
+DAYVAR=15 #$(date "+%d")
+# Insert your paths
+PYPATH=
+AOCURLPATH=
+REPOPATH=
+
+$PYPATH $AOCURLPATH $YEARVAR $DAYVAR;
+
+mkdir $REPOPATH/day$DAYVAR && cp $REPOPATH/template/* $REPOPATH/day$DAYVAR;
+echo -e ".SILENT:\nall:\n\t$PYPATH $AOCURLPATH $YEARVAR $DAYVAR -io | go run main.go testcases.go\n" > $REPOPATH/day$DAYVAR/Makefile
