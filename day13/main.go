3c9489586740bf4527e480ea857bc1fb2ddcaf0e
ezjanos <oskar.jansson@ericsson.com>
2020-12-14 09:15:50 +0100

    solves: day13 part 2

--- day13/main.go
+++ day13/main.go
@@ -22,6 +22,7 @@ func solutionLvl1(puzzle string, parameters map[string]int) (answer int) {
 	time := start
 Solved1:
 	for {
+
 		for _, b := range busIds {
 			if time%b == 0 {
 				answer = (time - start) * b
@@ -35,7 +36,10 @@ Solved1:
 }
 
 func departsAt(time, bus int) bool {
-	fmt.Println(bus)
+	// fmt.Println(bus)
+	if time == 0 || bus == 0 {
+		fmt.Println(time, bus)
+	}
 	return bus == -1 || (time%bus) == 0
 }
 
@@ -48,10 +52,32 @@ func all(l []bool) bool {
 	return true
 }
 
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
+
 // solutionLvl2 return answers for level 2
 func solutionLvl2(puzzle string, parameters map[string]int) (answer int) {
 	data := strings.Split(puzzle, "\n")
-	start := 0 //100000000000000
+
+	start := parameters["start"] //100000000000000
 	busIds := []int{}
 	for _, b := range strings.Split(data[1], ",") {
 		if b != "x" {
@@ -62,32 +88,34 @@ func solutionLvl2(puzzle string, parameters map[string]int) (answer int) {
 		}
 	}
 	time := start
-Solve2:
 	for {
-		check := []bool{}
-		for range busIds {
-			check = append(check, false)
-		}
-		for i, b := range busIds {
-			if departsAt(time, b) {
-				check[i] = true
-			}
-		}
-		if all(check) {
-			break Solve2
-		} else {
-			for i := range busIds {
-				check[i] = false
-			}
+		// Start at
+		// time     bus 7   bus 13  bus 59  bus 31  bus 19
+		// 1068780    .       .       .       .       .
+		// 1068781    D       .       .       .       .
+		// 1068782    .       D       .       .       .
+		if departsAt(time, busIds[0]) && departsAt(time+1, busIds[1]) {
+			break
 		}
 		time++
+	}
+Solve2:
+	for {
+		for i, b := range busIds {
+			if !departsAt(time+i, b) {
+				time += LCM(busIds[0], busIds[1], busIds[2:i]...)
+				continue Solve2
+			}
+		}
+		answer = time
+		break
 	}
 
 	return
 }
 
 var lvl1Parameters = map[string]int{}
-var lvl2Parameters = map[string]int{}
+var lvl2Parameters = map[string]int{"start": 100000000000000}
 
 func main() {
 	execution.Run(solutionLvl1, lvl1Parameters, solutionLvl2, lvl2Parameters, testcases)
--- day13/testcases.go
+++ day13/testcases.go
@@ -9,39 +9,39 @@ var testcases = tc.TestCases{
 		Input: `939
 7,13,x,x,59,x,31,19
 `,
-		Parameters:   map[string]int{},
+		Parameters:   map[string]int{"start": 1},
 		ExpectedLvl1: `295`,
-		ExpectedLvl2: `1068788`,
+		ExpectedLvl2: `1068781`,
 	},
 	{
 		Input: `939
 67,7,59,61
-`,
-		Parameters:   map[string]int{},
+	`,
+		Parameters:   map[string]int{"start": 1},
 		ExpectedLvl1: ``,
 		ExpectedLvl2: `754018`,
 	},
 	{
 		Input: `939
 67,x,7,59,61
-`,
-		Parameters:   map[string]int{},
+	`,
+		Parameters:   map[string]int{"start": 1},
 		ExpectedLvl1: ``,
 		ExpectedLvl2: `779210`,
 	},
 	{
 		Input: `939
 67,7,x,59,61
-`,
-		Parameters:   map[string]int{},
+	`,
+		Parameters:   map[string]int{"start": 1},
 		ExpectedLvl1: ``,
 		ExpectedLvl2: `1261476`,
 	},
 	{
 		Input: `939
 1789,37,47,1889
-`,
-		Parameters:   map[string]int{},
+	`,
+		Parameters:   map[string]int{"start": 1000000000},
 		ExpectedLvl1: ``,
 		ExpectedLvl2: `1202161486`,
 	},
