7b66fe9e6a68145b4aeffb5c03247ed6cea677f6
ezjanos <oskar.jansson@ericsson.com>
2020-12-23 07:05:25 +0100

    updates: day16part2

--- day16/main.go
+++ day16/main.go
@@ -20,13 +20,12 @@ func (r Rule) ValidateLeft(i int) bool {
 func (r Rule) ValidateRight(i int) bool {
 	return cmp.InBounds(r.RightLo, i, r.RightHi, true) == cmp.Within
 }
-
-func (r Rule) ValidateBoth(i int) bool {
-	return r.ValidateLeft(i) && r.ValidateRight(i)
+func (r Rule) Validate(i int) bool {
+	return r.ValidateLeft(i) || r.ValidateRight(i)
 }
 
 func (r Rule) String() string {
-	return fmt.Sprintf("%s: %d-%d or %d-%d", r.Title, r.LeftLo, r.LeftHi, r.RightLo, r.RightHi)
+	return fmt.Sprintf("%s", r.Title)
 }
 
 func data(puzzle string) ([]Rule, []int, [][]int) {
@@ -166,28 +165,100 @@ func solutionLvl1(puzzle string, parameters map[string]int) (answer int) {
 	return
 }
 
-// solutionLvl2 return answers for level 2
-func solutionLvl2(puzzle string, parameters map[string]int) (answer int) {
-	rules, _, tickets := data2(puzzle)
-	matches := map[int][]string{}
-
-	for _, t := range tickets {
-		for i, num := range t {
-			any := false
-			rule := rules[0]
-			for _, r := range rules {
-				if r.ValidateLeft(num) || r.ValidateRight(num) {
-					any = true
-					rule = r
-					break
-				}
-			}
-			if !any {
-				matches[i] = append(matches[i], rule.Title)
+func isValid(rules []Rule, ticket []int) bool {
+	for _, value := range ticket {
+		for _, r := range rules {
+			if r.Validate(value) {
+				return true
 			}
 		}
 	}
-	fmt.Println(matches)
+	return false
+}
+
+func setAdd(possible *map[int][]Rule, i int, r Rule) bool {
+	for _, rule := range (*possible)[i] {
+		if r == rule {
+			return false
+		}
+	}
+	(*possible)[i] = append((*possible)[i], r)
+	return true
+}
+
+// solutionLvl2 return answers for level 2
+func solutionLvl2(puzzle string, parameters map[string]int) (answer int) {
+	rules, ourTicket, tickets := data2(puzzle)
+	valid := [][]int{ourTicket}
+	for _, ticket := range tickets {
+		if isValid(rules, ticket) {
+			valid = append(valid, ticket)
+		}
+	}
+
+	matches := map[int][]Rule{}
+	used := map[Rule]bool{}
+	for {
+		for _, r := range rules {
+			_, ok := used[r]
+			if ok {
+				continue
+			}
+			for i := 0; i < len(ourTicket); i++ {
+				count := 0
+				for _, ticket := range valid {
+					if r.Validate(ticket[i]) {
+						count++
+					}
+				}
+				if count == len(valid) {
+					setAdd(&matches, i, r)
+					used[r] = true
+				}
+			}
+		}
+		if len(used) == len(rules) {
+			break
+		}
+
+	}
+
+	chosen := map[Rule]int{}
+	for {
+		clean := false
+		rule := Rule{}
+		for col, candidates := range matches {
+			if len(candidates) == 1 {
+				chosen[candidates[0]] = col
+				clean = true
+				rule = candidates[0]
+				candidates = []Rule{}
+				delete(matches, col)
+			}
+		}
+		if clean {
+			for i, candidates := range matches {
+				for j, c := range candidates {
+					if c == rule {
+						// TODO remove rule from candidates
+						candidates[j] = candidates[len(candidates)-1]
+						candidates = candidates[:len(candidates)-1]
+						matches[i] = candidates
+						break
+					}
+				}
+			}
+		}
+		if len(matches) == 0 {
+			break
+		}
+	}
+	answer = 1
+	for k, v := range chosen {
+		if strings.HasPrefix(k.Title, "departure") {
+			answer *= ourTicket[v]
+		}
+	}
 	return
 }
 
--- day16/testcases.go
+++ day16/testcases.go
@@ -24,4 +24,22 @@ nearby tickets:
 		ExpectedLvl1: `71`,
 		ExpectedLvl2: ``,
 	},
+	{
+		Input: `class: 0-1 or 4-19
+departure row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+`,
+
+		Parameters:   map[string]int{},
+		ExpectedLvl1: ``,
+		ExpectedLvl2: `11`,
+	},
 }
